712b42d0280aae4f0d029733813d19dcd77211eb
lilianabarrosc <lilianabarrosc@gmail.com>
2016-11-06 01:14:28 -0300

    Modelo de capas
    Creacion de entidades y carpetas. Entidad auto-atributos completa

---
+++ ReporteVentaVehiculos/ReporteVentaVehiculos/Entidades/Auto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReporteVentaVehiculos.Entidades
+{
+    public class Auto
+    {
+        public Auto()
+        {}
+
+        //atributos de la entidad Auto (ctrl+re para controladores)
+        private int _idauto;
+        private string _marca;
+        private string _modelo;
+        private string _tipoCombustible;
+        private int _año;
+
+        public int Idauto
+        {
+            get { return _idauto; }
+            set { _idauto = value; }
+        }
+
+        public string Marca
+        {
+            get { return _marca; }
+            set { _marca = value; }
+        }
+
+        public string Modelo
+        {
+            get { return _modelo; }
+            set { _modelo = value; }
+        }
+
+        public string TipoCombustible
+        {
+            get { return _tipoCombustible; }
+            set { _tipoCombustible = value; }
+        }
+
+        public int Año
+        {
+            get { return _año; }
+            set { _año = value; }
+        }
+    }
+}
